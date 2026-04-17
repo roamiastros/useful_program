32b625c450bb6095fe738498354180339c1a715b
samiawdeh <roamiastros@gmail.com>
2026-04-17 08:50:36 -0500

    added meow time baby

--- usefulLib.py
+++ usefulLib.py
@@ -123,9 +123,12 @@ def iNeedMyKeyboardRandomlyMashedForSomeReasonAndICantDoItMyself(num=50):
     for i in range(num):
         keyboard.press(random.choice(letters))
 
-        
-    
+def meowTimeBaby(min_interval=30,max_interval=60):
+    subprocess.Popen(f"pythonw meow.py {min_interval} {max_interval}", cwd="meow")
 
+if __name__ == '__main__':
+    meowTimeBaby(min_interval=30,max_interval=60)
+    print("works")
         
         
         
