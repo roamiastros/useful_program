9a33d882a84a210c2ade1ba793389bd75146a4fb
samiawdeh <roamiastros@gmail.com>
2026-04-17 08:38:06 -0500

    added meow.py command line

--- meow/meow.py
+++ meow/meow.py
@@ -1,3 +1,4 @@
+import sys
 from playsound3 import playsound
 import random
 import time
@@ -8,8 +9,18 @@ max_length=60#in seconds
 
 while True:
     try:
+        if sys.argv[1] and sys.argv[2]:
+            min_length=int(sys.argv[1])
+            max_length=int(sys.argv[2])
+            print(min_length," ",max_length)
+    except IndexError:
+        pass
+    try:
         interval=random.randrange(min_length, max_length+1)
         time.sleep(interval)
         playsound("meow.mp3")
-    except:
-        subprocess.run("msg * meow.py crashed", shell=True)
+    except Exception as e:
+        subprocess.run(f"msg * meow.py crashed, {e}", shell=True)
+        break
+
+quit()
